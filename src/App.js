d2f9d3c4b4befc848491246f8bc088310a48c64d
David Hoang <anhnhph33991@fpt.edu.vn>
2023-07-30 20:28:16 +0700

    components

--- src/App.js
+++ src/App.js
@@ -2,27 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import Header from './Components/Header/Header';
-
+import MyComponents from './components/MyComponents';
 
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
-  const name = 'Hello World';
 
   return (
-    <div className="App">
-      <Header/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi {name}
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Tăng</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Giảm</button>
-      </header>
+    
+    <div>
+      Hello World
+      <MyComponents></MyComponents>
     </div>
+
+
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Hi Hoang Anh
+    //     </p>
+    //     <div>Count = {count}</div>
+    //     <button onClick={() => dispatch(increaseCounter())}>Tăng</button>
+    //     <button onClick={() => dispatch(decreaseCounter())}>Giảm</button>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/components/MyComponents.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponents extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>Save new file</button>
+                <div>{Math.random()}</div>
+            </div>
+        )
+    }
+}
+
+export default MyComponents;
